1a6c26592e0f5fc98081845a788eaf2e8fe43efb
Absk <you@example.com>
2023-04-26 11:10:02 +0545

    first commit

---
+++ after/plugin/nvimtree.lua
@@ -0,0 +1,19 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- OR setup with some options
+require("nvim-tree").setup({
+    sort_by = "case_sensitive",
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+})
+
+
+vim.keymap.set("n", "<C-b>", "<cmd>NvimTreeToggle<CR>")
